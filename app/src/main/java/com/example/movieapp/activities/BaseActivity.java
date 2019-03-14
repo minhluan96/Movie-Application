e6bab162635dd2511da24449f8d67fe9bcf62f03
Luan Do <minhluan@Luans-MBP.net.fpt>
2019-03-14 15:29:52 +0700

    [ANDROID] remove kotlin lib

--- app/src/main/java/com/example/movieapp/activities/BaseActivity.java
+++ app/src/main/java/com/example/movieapp/activities/BaseActivity.java
@@ -14,8 +14,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.example.movieapp.R;
 
 public class BaseActivity extends AppCompatActivity {
 
